0c19643218c2d594e45819060b0abefdbd155621
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:51:10 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,6 +1,5 @@
 export default [
 { href: './html/Buckshot Roulette/index.html', imgSrc: 'buckshot roulette.jpeg', name: 'Buckshot Roulette' , 'page': 5, 'id': 763 },
-{ href: './html/Rubix/index.html', imgSrc: 'rubix.jpeg', name: 'Rubix' , 'page': 5, 'id': 939 },
 { href: './html/Rodha/index.html', imgSrc: 'rodha.jpeg', name: 'Rodha' , 'page': 5, 'id': 931 },
 { href: './html/Orcs vs elves/index.html', imgSrc: 'orcs vs elves.jpeg', name: 'Orcs vs elves' , 'page': 5, 'id': 924 },
 { href: './html/One trick mage/index.html', imgSrc: 'one trick mage.jpeg', name: 'One trick mage' , 'page': 5, 'id': 912 },
